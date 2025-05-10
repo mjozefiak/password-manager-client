fde45daa92fc5e705557d2bb0ba6804cf8de08ca
Mikołaj Józefiak <mikolaj.jozefiak@evolpe.pl>
2025-05-10 13:30:51 +0200

    project init

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import App from './App.vue'
+
+
+createApp(App)
+    .use(PrimeVue)
+    .mount('#app')
