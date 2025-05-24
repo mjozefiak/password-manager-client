79cb4ad82624cbf2989d874d4a9f6e8771a312cb
Mikołaj Józefiak <mikolaj.jozefiak@evolpe.pl>
2025-05-24 15:54:13 +0200

    packages, login example

--- src/main.js
+++ src/main.js
@@ -1,10 +1,37 @@
-import './assets/main.css'
-
-import { createApp } from 'vue'
+import { createApp } from 'vue';
+import { createRouter, createMemoryHistory } from 'vue-router';
+import Button from 'primevue/button';
 import PrimeVue from 'primevue/config';
-import App from './App.vue'
+import Aura from '@primeuix/themes/aura';
+import App from './App.vue';
+import ToastService from 'primevue/toastservice';
+import LoginView from './view/LoginView.vue';
+import InputText from 'primevue/inputtext';
+import { Form } from '@primevue/forms';
 
+const routes = [{ path: '/login', component: LoginView }];
 
-createApp(App)
-    .use(PrimeVue)
-    .mount('#app')
+const router = createRouter({
+   history: createMemoryHistory(),
+   routes,
+});
+
+const app = createApp(App);
+
+app.use(PrimeVue, {
+   theme: {
+      preset: Aura,
+      options: {
+         prefix: 'p',
+         darkModeSelector: 'system',
+         cssLayer: false,
+      },
+   },
+});
+app.use(router);
+app.use(ToastService);
+app.component('Button', Button);
+app.component('InputText', InputText);
+app.component('Form', Form);
+
+app.mount('#app');
