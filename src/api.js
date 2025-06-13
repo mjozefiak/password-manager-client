faf6a2ed72c389dfe8ea4d5801153c20a3c15e5a
Mikołaj Józefiak <mikolaj.jozefiak@evolpe.pl>
2025-06-13 23:10:11 +0200

    fix

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import router from './router';
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:3000';
 
 const api = axios.create({
    baseURL: API_BASE_URL,
@@ -82,7 +82,7 @@ export const deleteValut = async ({ id }) => {
 
 export const login = async ({ email, password }) => {
    try {
-      const response = await api.post('/auth', { email, password });
+      const response = await api.post('/api/auth', { email, password });
       const { token } = response.data;
       localStorage.setItem('token', token);
       return token;
